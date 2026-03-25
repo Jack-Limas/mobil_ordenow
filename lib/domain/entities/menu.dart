e3c9a8e2481a66a6cc6644317802055d701edcdd
JuanMaya8 <juanmaya@campusucc.edu.co>
2026-03-25 17:09:34 -0500

    Code entities menu

---
+++ lib/domain/entities/menu.dart
@@ -0,0 +1,15 @@
+class Menu {
+  final String id;
+  final String name;
+  final double price;
+  final String category;
+  final bool available;
+
+  Menu({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.category,
+    required this.available,
+  });
+}
