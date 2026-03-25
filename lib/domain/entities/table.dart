23d7a14b27aa73bf12a3a5b43efa85693576008e
JuanMaya8 <juanmaya@campusucc.edu.co>
2026-03-25 17:10:12 -0500

    Code entities table

---
+++ lib/domain/entities/table.dart
@@ -0,0 +1,11 @@
+class TableEntity {
+  final String id;
+  final int number;
+  final bool occupied;
+
+  TableEntity({
+    required this.id,
+    required this.number,
+    required this.occupied,
+  });
+}
