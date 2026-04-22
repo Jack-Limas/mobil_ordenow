a917da3df7e9b39d45d722064f1c54f537a32861
JuanMaya8 <juanmaya@campusucc.edu.co>
2026-04-22 15:08:49 -0500

    Expand menu entity fields

--- lib/domain/entities/menu.dart
+++ lib/domain/entities/menu.dart
@@ -1,15 +1,21 @@
 class Menu {
   final String id;
   final String name;
+  final String description;
   final double price;
   final String category;
   final bool available;
+  final bool recommended;
+  final List<String> tags;
 
   Menu({
     required this.id,
     required this.name,
+    required this.description,
     required this.price,
     required this.category,
     required this.available,
+    required this.recommended,
+    required this.tags,
   });
 }
