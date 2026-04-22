764421f87f98dbbdcf32849da2009fc54c2ce583
JuanMaya8 <juanmaya@campusucc.edu.co>
2026-04-22 15:08:39 -0500

    Expand user entity fields

--- lib/domain/entities/user.dart
+++ lib/domain/entities/user.dart
@@ -1,15 +1,21 @@
 class User {
   final String id;
   final String email;
-  final String name;
+  final String fullName;
+  final String password;
   final List<String> allergies;
   final List<String> preferences;
+  final DateTime createdAt;
+  final DateTime updatedAt;
 
   User({
     required this.id,
     required this.email,
-    required this.name,
+    required this.fullName,
+    required this.password,
     required this.allergies,
     required this.preferences,
+    required this.createdAt,
+    required this.updatedAt,
   });
 }
