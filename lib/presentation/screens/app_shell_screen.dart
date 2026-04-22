4327c1a6e9f02a68ee7ff6874edd0534a0bc978c
JuanMaya8 <juanmaya@campusucc.edu.co>
2026-04-22 17:22:24 -0500

    Route shell between demo experiences

--- lib/presentation/screens/app_shell_screen.dart
+++ lib/presentation/screens/app_shell_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../providers/ai_provider.dart';
+import '../providers/app_demo_provider.dart';
+import '../providers/order_provider.dart';
+import 'admin_demo_screen.dart';
+import 'customer_demo_screen.dart';
 import 'welcome_screen.dart';
 
 class AppShellScreen extends StatelessWidget {
@@ -7,6 +13,26 @@ class AppShellScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const WelcomeScreen();
+    final demo = context.watch<AppDemoProvider>();
+
+    switch (demo.experience) {
+      case DemoExperience.customer:
+        return const CustomerDemoScreen();
+      case DemoExperience.admin:
+        return const AdminDemoScreen();
+      case DemoExperience.welcome:
+        return WelcomeScreen(
+          onCustomerDemo: () {
+            context.read<OrderProvider>().clearDemoState();
+            context.read<AiProvider>().resetConversation();
+            context.read<AppDemoProvider>().openCustomerDemo();
+          },
+          onAdminDemo: () {
+            context.read<OrderProvider>().clearDemoState();
+            context.read<AiProvider>().resetConversation();
+            context.read<AppDemoProvider>().openAdminDemo();
+          },
+        );
+    }
   }
 }
