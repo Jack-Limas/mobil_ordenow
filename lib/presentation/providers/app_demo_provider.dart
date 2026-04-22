2d13948f7b35fc15624e7001e7b70fe456e5116f
JuanMaya8 <juanmaya@campusucc.edu.co>
2026-04-22 17:46:59 -0500

    Extend demo flow states for auth screens

--- lib/presentation/providers/app_demo_provider.dart
+++ lib/presentation/providers/app_demo_provider.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 enum DemoExperience {
   welcome,
+  signIn,
+  signUp,
   customer,
   admin,
 }
@@ -12,8 +14,20 @@ class AppDemoProvider extends ChangeNotifier {
   DemoExperience get experience => _experience;
 
   bool get isWelcome => _experience == DemoExperience.welcome;
+  bool get isSignIn => _experience == DemoExperience.signIn;
+  bool get isSignUp => _experience == DemoExperience.signUp;
   bool get isCustomer => _experience == DemoExperience.customer;
   bool get isAdmin => _experience == DemoExperience.admin;
+
+  void openSignIn() {
+    _experience = DemoExperience.signIn;
+    notifyListeners();
+  }
+
+  void openSignUp() {
+    _experience = DemoExperience.signUp;
+    notifyListeners();
+  }
 
   void openCustomerDemo() {
     _experience = DemoExperience.customer;
