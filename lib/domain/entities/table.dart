376392f08086413ad6823afff00bf4299f1fa4e7
JuanMaya8 <juanmaya@campusucc.edu.co>
2026-04-22 15:09:23 -0500

    Add payment flag to table entity

--- lib/domain/entities/table.dart
+++ lib/domain/entities/table.dart
@@ -2,10 +2,12 @@ class TableEntity {
   final String id;
   final int number;
   final bool occupied;
+  final bool needsPayment;
 
   TableEntity({
     required this.id,
     required this.number,
     required this.occupied,
+    required this.needsPayment,
   });
 }
