7b0ffd8703babae2079955e52c7ab7b02889016d
JuanMaya8 <juanmaya@campusucc.edu.co>
2026-04-22 15:08:28 -0500

    Add settings box to Hive service

--- lib/data/datasources/local/hive_service.dart
+++ lib/data/datasources/local/hive_service.dart
@@ -1,20 +1,23 @@
 import 'package:hive_flutter/hive_flutter.dart';
 
+import '../../../core/utils/constants.dart';
+
 class HiveService {
   static Future<void> init() async {
-    await Hive.initFlutter();
-
-    await Hive.openBox('user');
-    await Hive.openBox('order');
-    await Hive.openBox('menu');
-    await Hive.openBox('table');
+    await Hive.openBox(HiveBoxes.settings);
+    await Hive.openBox(HiveBoxes.user);
+    await Hive.openBox(HiveBoxes.order);
+    await Hive.openBox(HiveBoxes.menu);
+    await Hive.openBox(HiveBoxes.table);
   }
 
-  static Box getUserBox() => Hive.box('user');
+  static Box get settingsBox => Hive.box(HiveBoxes.settings);
 
-  static Box getOrderBox() => Hive.box('order');
+  static Box getUserBox() => Hive.box(HiveBoxes.user);
 
-  static Box getMenuBox() => Hive.box('menu');
+  static Box getOrderBox() => Hive.box(HiveBoxes.order);
 
-  static Box getTableBox() => Hive.box('table');
+  static Box getMenuBox() => Hive.box(HiveBoxes.menu);
+
+  static Box getTableBox() => Hive.box(HiveBoxes.table);
 }
